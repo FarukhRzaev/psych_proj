0771501ddb2d89a718d5638cbcfd14a54b2326cd
farukhrzaev <farukh.rzaev@gmail.com>
2023-12-15 03:38:29 +0300

    Везде реализовал CBV

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.main_page, name="main_page"),
-    path("about-me/", views.about_me, name="about_me"),
-    path("posts/", views.get_info_all_posts, name="all_posts"),
+    path("", views.MainPage.as_view(), name="main_page"),
+    path("about-me/", views.AboutMe.as_view(), name="about_me"),
+    path("posts/", views.AllPosts.as_view(), name="all_posts"),
     path("posts/<slug:slug>/", views.DetailPostView.as_view(), name="post_details"),
     path(
         "hashtags/<slug:slug>/",
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,43 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from django.views.generic.base import TemplateView
 from .models import Post, Hashtag, Gallery
 
 
-def main_page(request):
-    posts = list(Post.objects.order_by("-date"))
-    context = {
-        "posts": posts[:3],
-        "image": Gallery.objects.get(slug="main-page"),
-    }
-    return render(request, "blog/main_page.html", context)
+class MainPage(ListView):
+    template_name = "blog/main_page.html"
+    model = Post
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filter_qs = queryset.order_by("-date")[:3]
+        return filter_qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["image"] = Gallery.objects.get(slug="main-page")
+        return context
 
 
-def about_me(request):
-    context = {"image": Gallery.objects.get(slug="about-me")}
-    return render(request, "blog/about_me.html", context)
+class AboutMe(TemplateView):
+    template_name = "blog/about_me.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["image"] = Gallery.objects.get(slug="about-me")
+        return context
 
 
-def get_info_all_posts(request):
-    context = {"posts": Post.objects.order_by("-date")}
-    return render(request, "blog/list_post.html", context)
+class AllPosts(ListView):
+    template_name = "blog/list_post.html"
+    model = Post
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filter_qs = queryset.order_by("-date")
+        return filter_qs
 
 
 class DetailPostView(DetailView):
